6731c03b420e022c3535fc1f52a7844d43f884f3
Akianonymus <anonymus.aki@gmail.com>
2021-08-23 10:17:39 +0530

    update_nvchad: Restore last repo state on failure of git pull

--- lua/nvchad/init.lua
+++ lua/nvchad/init.lua
@@ -223,6 +223,56 @@ M.update_nvchad = function()
    local current_config = require("core.utils").load_config()
    local update_url = current_config.options.update_url or "https://github.com/NvChad/NvChad"
    local update_branch = current_config.options.update_branch or "main"
+   local current_sha, backup_sha = "", ""
+   local function restore_repo_state()
+      -- on failing, restore to the last repo state, including untracked files
+      vim.fn.system(
+         "git -C "
+            .. config_path
+            .. " reset --hard "
+            .. current_sha
+            .. " ; git -C "
+            .. config_path
+            .. " cherry-pick -n "
+            .. backup_sha
+            .. " ; git reset"
+      )
+   end
+
+   local function rm_backup()
+      if not pcall(os.remove, config_file_backup) then
+         echo { { "Warning: Failed to remove backup chadrc, remove manually.", "WarningMsg" } }
+         echo { { "Path: ", "WarningMsg" }, { config_file_backup } }
+      end
+   end
+
+   -- first try to fetch contents of config, this will make sure it is readable and taking backup of its contents
+   local config_contents = utils.file("r", config_file)
+   -- also make a local backup in ~/.config/nvim, will be removed when config is succesfully restored
+   utils.file("w", config_file_backup, config_contents)
+   -- write original config file with its contents, will make sure charc is writable, this maybe overkill but a little precaution always helps
+   utils.file("w", config_file, config_contents)
+
+   -- save the current sha and check if config folder is a valid git directory
+   local valid_git_dir = true
+   current_sha = vim.fn.system("git -C " .. config_path .. " rev-parse HEAD")
+
+   if vim.api.nvim_get_vvar "shell_error" == 0 then
+      vim.fn.system("git -C " .. config_path .. " commit -a -m 'tmp'")
+      backup_sha = vim.fn.system("git -C " .. config_path .. " rev-parse HEAD")
+      if vim.api.nvim_get_vvar "shell_error" ~= 0 then
+         valid_git_dir = false
+      end
+   else
+      valid_git_dir = false
+   end
+
+   if not valid_git_dir then
+      restore_repo_state()
+      rm_backup()
+      echo { { "Error: " .. config_path .. " is not a git directory.\n" .. current_sha .. backup_sha, "ErrorMsg" } }
+      return
+   end
 
    -- ask the user for confirmation to update because we are going to run git reset --hard
    echo { { "Url: ", "Title" }, { update_url } }
@@ -242,16 +292,11 @@ M.update_nvchad = function()
    local ans = string.lower(vim.fn.input "-> ") == "y"
    utils.clear_cmdline()
    if not ans then
+      restore_repo_state()
+      rm_backup()
       echo { { "Update cancelled!", "Title" } }
       return
    end
-
-   -- first try to fetch contents of config, this will make sure it is readable and taking backup of its contents
-   local config_contents = utils.file("r", config_file)
-   -- also make a local backup in ~/.config/nvim, will be removed when config is succesfully restored
-   utils.file("w", config_file_backup, config_contents)
-   -- write original config file with its contents, will make sure charc is writable, this maybe overkill but a little precaution always helps
-   utils.file("w", config_file, config_contents)
 
    -- function that will executed when git commands are done
    local function update_exit(_, code)
@@ -260,10 +305,7 @@ M.update_nvchad = function()
          utils.file("w", config_file, config_contents)
       end) then
          -- config restored succesfully, remove backup file that was created
-         if not pcall(os.remove, config_file_backup) then
-            echo { { "Warning: Failed to remove backup chadrc, remove manually.", "WarningMsg" } }
-            echo { { "Path: ", "WarningMsg" }, { config_file_backup } }
-         end
+         rm_backup()
       else
          echo { { "Error: Restoring " .. config_name .. " failed.\n", "ErrorMsg" } }
          echo { { "Backed up " .. config_name .. " path: " .. config_file_backup .. "\n\n", "None" } }
@@ -274,14 +316,17 @@ M.update_nvchad = function()
          vim.cmd "bd!"
          echo { { "NvChad succesfully updated.\n", "String" } }
       else
+         restore_repo_state()
          echo { { "Error: NvChad Update failed.\n", "ErrorMsg" } }
+         echo { { "Local changes were restored." } }
       end
    end
 
-   -- git commands that will executed, reset in case config was modfied
+   -- reset in case config was modified
+   vim.fn.system("git -C " .. config_path .. " reset --hard " .. current_sha)
    -- use --rebase, to not mess up if the local repo is outdated
    local update_script = table.concat({
-      "git reset --hard && git pull --set-upstream",
+      "git pull --set-upstream",
       update_url,
       update_branch,
       "--rebase",

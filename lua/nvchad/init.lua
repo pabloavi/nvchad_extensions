505b27136bfe36aceb2edfeca7d55aa6c23ce492
Akianonymus <anonymus.aki@gmail.com>
2021-08-26 11:34:08 +0530

    theme_switcher: Update theme switcher for feline.nvim
    feline doesn't need to be reloaded, just reload the config and done

--- lua/nvchad/init.lua
+++ lua/nvchad/init.lua
@@ -145,49 +145,12 @@ M.reload_theme = function(theme_name)
    if
       not reload_plugin {
          "plugins.configs.bufferline",
-         "galaxyline",
          "plugins.configs.statusline",
       }
    then
       print "Error: Not able to reload all plugins."
       return false
    end
-
-   -- yes, this is very hacky, but due to new_async in
-   -- https://github.com/glepnir/galaxyline.nvim/blob/main/lua/galaxyline/provider.lua#L5-L36
-   -- it doesn't work properly and some statusline stuff dissapears
-   local vcs = require "galaxyline.provider_vcs"
-   local fileinfo = require "galaxyline.provider_fileinfo"
-   local buffer = require "galaxyline.provider_buffer"
-   local extension = require "galaxyline.provider_extensions"
-   local whitespace = require "galaxyline.provider_whitespace"
-   local lspclient = require "galaxyline.provider_lsp"
-   _G.galaxyline_providers = {
-      BufferIcon = buffer.get_buffer_type_icon,
-      BufferNumber = buffer.get_buffer_number,
-      FileTypeName = buffer.get_buffer_filetype,
-      GitBranch = vcs.get_git_branch,
-      DiffAdd = vcs.diff_add,
-      DiffModified = vcs.diff_modified,
-      DiffRemove = vcs.diff_remove,
-      LineColumn = fileinfo.line_column,
-      FileFormat = fileinfo.get_file_format,
-      FileEncode = fileinfo.get_file_encode,
-      FileSize = fileinfo.get_file_size,
-      FileIcon = fileinfo.get_file_icon,
-      FileName = fileinfo.get_current_file_name,
-      SFileName = fileinfo.filename_in_special_buffer,
-      LinePercent = fileinfo.current_line_percent,
-      ScrollBar = extension.scrollbar_instance,
-      VistaPlugin = extension.vista_nearest,
-      WhiteSpace = whitespace.get_item,
-      GetLspClient = lspclient.get_lsp_client,
-   }
-   local diagnostic = require "galaxyline.provider_diagnostic"
-   _G.galaxyline_providers.DiagnosticError = diagnostic.get_diagnostic_error
-   _G.galaxyline_providers.DiagnosticWarn = diagnostic.get_diagnostic_warn
-   _G.galaxyline_providers.DiagnosticHint = diagnostic.get_diagnostic_hint
-   _G.galaxyline_providers.DiagnosticInfo = diagnostic.get_diagnostic_info
 
    return true
 end

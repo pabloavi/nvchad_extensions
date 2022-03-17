e7846f8701a7896da36737573027839f5abf0752
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-03-17 16:14:04 -0400

    working with telescope ext

--- lua/core/terminal.lua
+++ lua/core/terminal.lua
@@ -4,32 +4,36 @@ local get_cmds = function(window_opts)
    local get_dims = function(direction)
       local direction_switch = direction == "horizontal"
       local direction_func = direction_switch and vim.api.nvim_win_get_height or vim.api.nvim_win_get_width
-      local assignment = direction_switch and "split_height" or "vsplit_width"
       return math.floor(direction_func(0) * window_opts)
    end
+   local dims = {
+      horizontal = get_dims("horizontal"),
+      vertical = get_dims("vertical"),
+   }
    local cmds = {
       horizontal = {
          existing = "rightbelow vsplit",
-         new = "botright " .. get_dims "horizontal" .. "split",
+         new = "botright " .. dims.horizontal .. " split",
          resize = "resize",
       },
       vertical = {
          existing = "rightbelow split",
-         new = "botright " .. get_dims "vertical" .. "vsplit",
+         new = "botright " .. dims.vertical .. " vsplit",
          resize = "vertical resize",
       },
    }
-   return cmds
+   return cmds, dims
 end
 
 local function on_action(chadterms)
    Globals.chadterms = chadterms
 end
 
-local function on_new_buf()
+local function on_new_buf(opts)
    local bufs = vim.api.nvim_list_bufs()
    local term_buf_id = bufs[#bufs]
-   vim.api.nvim_buf_set_var(term_buf_id, "term_type", true)
+   vim.api.nvim_buf_set_var(term_buf_id, "term_type", opts.direction)
+   vim.api.nvim_buf_set_var(term_buf_id, "term_size", opts.size)
    vim.api.nvim_input "i" --term enter
    return term_buf_id
 end
@@ -40,18 +44,22 @@ local function on_new_win()
    vim.api.nvim_set_current_win(term_win_id)
    return term_win_id
 end
-local function spawn(spawn_cmd, type)
+local function spawn(spawn_cmd, type, opts)
    vim.cmd(spawn_cmd)
-   return type == "win" and on_new_win() or type == "buf" and on_new_buf()
+   print(spawn_cmd)
+   return type == "win" and on_new_win() or type == "buf" and on_new_buf(opts)
 end
 
 M.new_or_toggle = function(direction, window_opts)
    local chadterms = Globals.chadterms
-   local cmds = get_cmds(window_opts)
+   local cmds, dims = get_cmds(window_opts)
 
    local function new_term()
       local term_win_id = spawn(cmds[direction]["new"], "win")
-      local term_buf_id = spawn("term", "buf")
+      local term_buf_id = spawn("term", "buf", {
+         direction=direction,
+         size=dims[direction]
+      })
       chadterms[direction][1] = { win = term_win_id, buf = term_buf_id }
    end
 

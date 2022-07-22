fd46bb0ed0eab9584e75742bf18d81467b1b6faa
siduck <siduck@tutanota.com>
2022-07-22 16:01:01 +0000

    format files

--- lua/nvchad/init.lua
+++ lua/nvchad/init.lua
@@ -8,27 +8,27 @@ M.change_theme = require "nvchad.change_theme"
 
 -- clear command line from lua
 M.clear_cmdline = function()
-   vim.defer_fn(function()
-      vim.cmd "echo"
-   end, 0)
+  vim.defer_fn(function()
+    vim.cmd "echo"
+  end, 0)
 end
 
 -- wrapper to use vim.api.nvim_echo
 -- table of {string, highlight}
 -- e.g echo({{"Hello", "Title"}, {"World"}})
 M.echo = function(opts)
-   if opts == nil or type(opts) ~= "table" then
-      return
-   end
-   vim.api.nvim_echo(opts, false, {})
+  if opts == nil or type(opts) ~= "table" then
+    return
+  end
+  vim.api.nvim_echo(opts, false, {})
 end
 
 -- clear last echo using feedkeys (this is a bit hacky)
 M.clear_last_echo = function()
-   -- wrap this with inputsave and inputrestore just in case
-   fn.inputsave()
-   vim.api.nvim_feedkeys(":", "nx", true)
-   fn.inputrestore()
+  -- wrap this with inputsave and inputrestore just in case
+  fn.inputsave()
+  vim.api.nvim_feedkeys(":", "nx", true)
+  fn.inputrestore()
 end
 
 -- a wrapper for running terminal commands that also handles errors
@@ -36,19 +36,19 @@ end
 -- 2nd arg - a boolean to indicate whether to print possible errors
 -- returns the result if successful, nil otherwise
 M.cmd = function(cmd, print_error)
-   -- check if NvChad is running on windows and pipe the command through cmd.exe
-   if vim.fn.has 'win32' == 1 then
-      cmd = { 'cmd.exe', '/C', cmd }
-   end
+  -- check if NvChad is running on windows and pipe the command through cmd.exe
+  if vim.fn.has "win32" == 1 then
+    cmd = { "cmd.exe", "/C", cmd }
+  end
 
-   local result = fn.system(cmd)
-   if vim.api.nvim_get_vvar "shell_error" ~= 0 then
-      if print_error then
-         vim.api.nvim_err_writeln("Error running command:\n" .. cmd .. "\nError message:\n" .. result)
-      end
-      return nil
-   end
-   return result
+  local result = fn.system(cmd)
+  if vim.api.nvim_get_vvar "shell_error" ~= 0 then
+    if print_error then
+      vim.api.nvim_err_writeln("Error running command:\n" .. cmd .. "\nError message:\n" .. result)
+    end
+    return nil
+  end
+  return result
 end
 
 -- 1st arg - r or w
@@ -56,47 +56,47 @@ end
 -- 3rd arg - content if 1st arg is w
 -- return file data on read, nothing on write
 M.file = function(mode, filepath, content)
-   local data
-   local base_dir = fn.fnamemodify(filepath, ":h")
-   -- check if file exists in filepath, return false if not
-   if mode == "r" and fn.filereadable(filepath) == 0 then
-      return false
-   end
-   -- check if directory exists, create it and all parents if not
-   if mode == "w" and fn.isdirectory(base_dir) == 0 then
-      fn.mkdir(base_dir, "p")
-   end
-   local fd = assert(vim.loop.fs_open(filepath, mode, 438))
-   local stat = assert(vim.loop.fs_fstat(fd))
-   if stat.type ~= "file" then
-      data = false
-   else
-      if mode == "r" then
-         data = assert(vim.loop.fs_read(fd, stat.size, 0))
-      else
-         assert(vim.loop.fs_write(fd, content, 0))
-         data = true
-      end
-   end
-   assert(vim.loop.fs_close(fd))
-   return data
+  local data
+  local base_dir = fn.fnamemodify(filepath, ":h")
+  -- check if file exists in filepath, return false if not
+  if mode == "r" and fn.filereadable(filepath) == 0 then
+    return false
+  end
+  -- check if directory exists, create it and all parents if not
+  if mode == "w" and fn.isdirectory(base_dir) == 0 then
+    fn.mkdir(base_dir, "p")
+  end
+  local fd = assert(vim.loop.fs_open(filepath, mode, 438))
+  local stat = assert(vim.loop.fs_fstat(fd))
+  if stat.type ~= "file" then
+    data = false
+  else
+    if mode == "r" then
+      data = assert(vim.loop.fs_read(fd, stat.size, 0))
+    else
+      assert(vim.loop.fs_write(fd, content, 0))
+      data = true
+    end
+  end
+  assert(vim.loop.fs_close(fd))
+  return data
 end
 
 M.list_themes = function()
-   local default_themes = vim.fn.readdir(vim.fn.stdpath "data" .. "/site/pack/packer/start/base46/lua/base46/themes")
+  local default_themes = vim.fn.readdir(vim.fn.stdpath "data" .. "/site/pack/packer/start/base46/lua/base46/themes")
 
-   local custom_theme_ok = pcall(require, "custom.themes")
+  local custom_theme_ok = pcall(require, "custom.themes")
 
-   if custom_theme_ok then
-      local user_themes = fn.readdir(fn.stdpath "config" .. "/lua/custom/themes")
-      default_themes = vim.tbl_deep_extend("force", default_themes, user_themes)
-   end
+  if custom_theme_ok then
+    local user_themes = fn.readdir(fn.stdpath "config" .. "/lua/custom/themes")
+    default_themes = vim.tbl_deep_extend("force", default_themes, user_themes)
+  end
 
-   for index, theme in ipairs(default_themes) do
-      default_themes[index] = fn.fnamemodify(fn.fnamemodify(theme, ":t"), ":r")
-   end
+  for index, theme in ipairs(default_themes) do
+    default_themes[index] = fn.fnamemodify(fn.fnamemodify(theme, ":t"), ":r")
+  end
 
-   return default_themes
+  return default_themes
 end
 
 -- reload themes without restarting vim
@@ -117,14 +117,14 @@ M.snap_checkout = require "nvchad.updater.snap_checkout"
 M.snap_delete = require "nvchad.updater.snap_delete"
 
 M.write_data = function(old_data, new_data)
-   local file_fn = require("nvchad").file
-   local file = fn.stdpath "config" .. "/lua/custom/" .. "chadrc.lua"
-   local data = file_fn("r", file)
+  local file_fn = require("nvchad").file
+  local file = fn.stdpath "config" .. "/lua/custom/" .. "chadrc.lua"
+  local data = file_fn("r", file)
 
-   local content = string.gsub(data, old_data, new_data)
+  local content = string.gsub(data, old_data, new_data)
 
-   -- see if the find string exists in file
-   assert(file_fn("w", file, content))
+  -- see if the find string exists in file
+  assert(file_fn("w", file, content))
 end
 
 return M
--- lua/nvchad/reload_theme.lua
+++ lua/nvchad/reload_theme.lua
@@ -2,23 +2,23 @@
 -- if no theme name given then reload the current theme
 
 local function reload_theme(theme_name)
-   -- if theme name is empty or nil, then reload the current theme
-   if theme_name == nil or theme_name == "" then
-      theme_name = vim.g.nvchad_theme
-   end
+  -- if theme name is empty or nil, then reload the current theme
+  if theme_name == nil or theme_name == "" then
+    theme_name = vim.g.nvchad_theme
+  end
 
-   local default_themes = pcall(require, "base46.themes." .. theme_name)
-   local user_themes = pcall(require, "custom.themes." .. theme_name)
+  local default_themes = pcall(require, "base46.themes." .. theme_name)
+  local user_themes = pcall(require, "custom.themes." .. theme_name)
 
-   if not default_themes and not user_themes then
-      print("No such theme ( " .. theme_name .. " )")
-      return false
-   end
+  if not default_themes and not user_themes then
+    print("No such theme ( " .. theme_name .. " )")
+    return false
+  end
 
-   vim.g.nvchad_theme = theme_name
-   require("base46").load_all_highlights()
+  vim.g.nvchad_theme = theme_name
+  require("base46").load_all_highlights()
 
-   return true
+  return true
 end
 
 return reload_theme

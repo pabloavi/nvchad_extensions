045f8a4564e2a22115c9b1392b25e228f6ce8417
shaun-mathew <shaun-mathew@users.noreply.github.com>
2022-08-11 00:34:15 -0400

    Replaced vim.cmd autocmd call to nvim autocmd api call

--- lua/nvchad/reload_theme.lua
+++ lua/nvchad/reload_theme.lua
@@ -18,7 +18,7 @@ local function reload_theme(theme_name)
   vim.g.nvchad_theme = theme_name
   require("base46").load_all_highlights()
   
-  vim.cmd "doautocmd User NvChadThemeReload"
+  vim.api.nvim_exec_autocmds("User", { pattern = " NvChadThemeReload" })
   
   return true
 end

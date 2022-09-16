1f67c3c2dfca8ff545afd694eb76c9ef45169567
siduck <siduck@tutanota.com>
2022-09-16 08:50:55 +0530

    fix theme switcher erroring without chadrc

--- lua/nvchad/utils/config.lua
+++ lua/nvchad/utils/config.lua
@@ -3,7 +3,7 @@ local M = {}
 M.custom = {
   config_dir_rel = "lua/custom",
   config_dir_abs = vim.fn.stdpath "config" .. "/lua/custom",
-  default_chadrc_example_path = vim.fn.stdpath "config" .. "/examples/" .. "chadrc.lua",
+  default_chadrc_example_path = vim.fn.stdpath "data" .. "/site/pack/packer/opt/extensions/examples/chadrc.lua",
   default_chadrc_path = vim.fn.stdpath "config" .. "/lua/custom/" .. "chadrc.lua",
 }
 
